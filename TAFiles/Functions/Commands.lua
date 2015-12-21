96e851c6542c6e810faa9c7b7e59348a00b47899
CometWolf <paukatt@gmail.com>
2015-12-21 17:23:48 +0100

    Command computer scan and clear functionality & bugfixes
    Fixed layerbar import

--- TAFiles/Functions/Commands.lua
+++ TAFiles/Functions/Commands.lua
@@ -96,26 +96,22 @@ function checkProgress(fileName,tProgress,blueprint,auto)
   and fs.exists(fileName..".TAo")
   and not tProgress then
     tProgress = loadProgress(fileName)
-    if auto then
-      return tProgress
-    else
-      local button = window.text(
-        [[In-progress build of current blueprint found.
+    local button = window.text(
+      [[In-progress build of current blueprint found.
 layers ]]..tProgress.layers[1]..[[-]]..tProgress.layers[#tProgress.layers]..[[ 
 X: ]]..tProgress.x..[[ "]]..tProgress.dir.x..[["
 Y: ]]..tProgress.y..[[ "]]..tProgress.dir.y..[["
 Z: ]]..tProgress.z..[[ "]]..tProgress.dir.z..[["
 Load?]],
-        {
-          "Yes",
-          "No"
-        }
-      )
-      if button == "No" then
-        tProgress = {
-          dir = {}
-        }
-      end
+      {
+        "Yes",
+        "No"
+      }
+    )
+    if button == "No" then
+      tProgress = {
+        dir = {}
+      }
     end
   else
     tProgress = {
@@ -269,19 +265,18 @@ Load?]],
   return tProgress,fileName
 end
 
-function build(blueprint)
+function build(blueprint,clear)
   --builds the given blueprint layers
-  if not tFile.blueprint then
+  if not clear and not tFile.blueprint then
     if not dialogue.save"Blueprint must be saved locally prior to building" then
       window.text"Construction cancelled"
       return
     end
   end
   blueprint = blueprint or tBlueprint
-  local tOngoing 
-  tOngoing = checkProgress(tFile.blueprint)
+  local tOngoing = checkProgress(tFile.blueprint)
   if not tOngoing then
-    window.text"Construction cancelled."
+    window.text((clear and "Removal" or "Construction").." cancelled.")
     return
   else
     tOngoing = loadProgress(tFile.blueprint)
@@ -296,15 +291,23 @@ function build(blueprint)
     for nX,vX in pairs(layerCopy) do
       for nZ in pairs(vX) do
         local block = blueprint[nL][nX][nZ]
-        if block and block:match"[%lX]" then
-          if block == "X" then
-            commands.execAsync("setblock "..tOngoing.x + tonumber(dirX..nX-1).." "..tOngoing.y + tonumber(dirY..nL-1).." "..tOngoing.z + tonumber(dirZ..nZ-1).." 0")
-            blueprint[nL][nX][nZ] = nil
-          else
-            commands.execAsync("setblock "..tOngoing.x + tonumber(dirX..nX-1).." "..tOngoing.y + tonumber(dirY..nL-1).." "..tOngoing.z + tonumber(dirZ..nZ-1).." "..tBlueprint.colorSlots[block])
-            blueprint[nL][nX][nZ] = block:upper()
+        if block then
+          if clear then
+            if block ~= "X" then
+              commands.execAsync("setblock "..tOngoing.x + tonumber(dirX..nX-1).." "..tOngoing.y + tonumber(dirY..nL-1).." "..tOngoing.z + tonumber(dirZ..nZ-1).." 0")
+              blueprint[nL][nX][nZ] = block:lower()
+              loop = loop+1
+            end
+          elseif block:match"[%lX]" then
+            if block == "X" then
+              commands.execAsync("setblock "..tOngoing.x + tonumber(dirX..nX-1).." "..tOngoing.y + tonumber(dirY..nL-1).." "..tOngoing.z + tonumber(dirZ..nZ-1).." 0")
+              blueprint[nL][nX][nZ] = nil
+            else
+              commands.execAsync("setblock "..tOngoing.x + tonumber(dirX..nX-1).." "..tOngoing.y + tonumber(dirY..nL-1).." "..tOngoing.z + tonumber(dirZ..nZ-1).." "..tBlueprint.colorSlots[block])
+              blueprint[nL][nX][nZ] = block:upper()
+            end
+            loop = loop+1
           end
-          loop = loop+1
           if loop%10000 == 0 then
             blueprint:save(tFile.blueprint,true)
             scroll(nL,nX-math.floor(tTerm.canvas.tX/2),nZ-math.floor(tTerm.canvas.tZ/2),true,true)
@@ -317,5 +320,103 @@ function build(blueprint)
   end
   blueprint:save(tFile.blueprint,true)
   scroll()
-  window.text"Construction complete"
+  window.text((clear and "Removal" or "Construction").." complete.")
+end
+
+function scan(x1,y1,z1,x2,y2,z2)
+  if not (x1 and y1 and z1 and x2 and y2 and z2) then
+    local button, tRes, reInput = window.text(
+      "Input scan boundaries",
+      {
+        "Cancel",
+        "Ok",
+      },
+      {
+        {
+          name = "X1",
+          value = tPos.x,
+          accepted = "[%d+-]",
+        },
+        {
+          name = "Y1",
+          value = tPos.y,
+          accepted = "[%d+-]",
+        },
+        {
+          name = "Z1",
+          value = tPos.z,
+          accepted = "[%d+-]",
+        },
+        {
+          name = "X2",
+          value = tPos.x,
+          accepted = "[%d+-]",
+        },
+        {
+          name = "Y2",
+          value = tPos.y,
+          accepted = "[%d+-]",
+        },
+        {
+          name = "Z2",
+          value = tPos.z,
+          accepted = "[%d+-]",
+        },
+      },
+      false,
+      true
+    )
+    while button ~= "Cancel" do
+      if not tRes.X1 then
+        button, tRes, reInput = reInput"Missing X1 parameter!"
+      elseif not tRes.X2 then
+        button, tRes, reInput = reInput"Missing X2 parameter!"
+      elseif not tRes.Y1 then
+        button, tRes, reInput = reInput"Missing Y1 parameter!"
+      elseif not tRes.Y2 then
+        button, tRes, reInput = reInput"Missing Y2 parameter!"
+      elseif not tRes.Z1 then
+        button, tRes, reInput = reInput"Missing Z1 parameter!"
+      elseif not tRes.Z2 then
+        button, tRes, reInput = reInput"Missing Z2 parameter!"
+      else
+        x1 = math.min(tRes.X1,tRes.X2)
+        x2 = math.max(tRes.X1,tRes.X2)
+        y1 = math.min(tRes.Y1,tRes.Y2)
+        y2 = math.max(tRes.Y1,tRes.Y2)
+        z1 = math.min(tRes.Z1,tRes.Z2)
+        z2 = math.max(tRes.Z1,tRes.Z2)
+        break
+      end
+    end
+    if button == "Cancel" then
+      window.text"Scan cancelled."
+      return
+    end
+  end
+  local tBlocks = {}
+  local iColor = 1
+  loop = 1
+  for iY = 1,math.abs(y2-y1)+1 do
+    tBlueprint[iY] = tBlueprint[iY] or class.layer.new()
+    for iX = 1,math.abs(x2-x1)+1 do
+      for iZ = 1,math.abs(z2-z1)+1 do
+        local block = commands.getBlockInfo(x1+iX-1,y1+iY-1,z1+iZ-1)
+        if block.name ~= "minecraft:air" then
+          if not tBlocks[block.name] then
+            tBlocks[block.name] = colorKey[2^iColor]:upper()
+            iColor = (iColor < 16 and iColor+1 or 1)
+          end
+          tBlueprint[iY][iX][iZ] = tBlocks[block.name]
+          loop = loop+1
+          if loop >= 10 then
+            scroll(iY,iX-math.floor(tTerm.canvas.tX/2),iZ-math.floor(tTerm.canvas.tZ/2),true,true)
+            screen:refresh()
+            loop = 1
+          end
+        end
+      end
+    end
+  end
+  window.text"Scan complete."
 end
